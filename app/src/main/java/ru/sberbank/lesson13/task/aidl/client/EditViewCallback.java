295c318b4ba017177354e821a6d90d47d94ce618
w3dip <w3dipmail@gmail.com>
2019-03-10 00:04:55 +0300

    refactoring

--- app/src/main/java/ru/sberbank/lesson13/task/aidl/client/EditViewCallback.java
+++
@@ -1,5 +0,0 @@
-package ru.sberbank.lesson13.task.aidl.client;
-
-interface EditViewCallback {
-    void onValueChanged(String value);
-}
--- app/src/main/java/ru/sberbank/lesson13/task/aidl/client/MainActivity.java
+++ app/src/main/java/ru/sberbank/lesson13/task/aidl/client/MainActivity.java
@@ -11,47 +11,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import ru.sberbank.lesson13.task.aidl.contract.Data;
 import ru.sberbank.lesson13.task.aidl.contract.IRemoteDataService;
 
 public class MainActivity extends FragmentActivity implements FragmentB.OnFragmentInteractionListener {
     private IRemoteDataService remoteDataService;
-    //private static final String SERVICE_IS_STARTED = "isStarted";
-
-    private EditText editTextView;
-
-    //private boolean isStarted;
+    private ScheduledExecutorService scheduledThreadPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editTextView = findViewById(R.id.myEditText);
-        /*if (savedInstanceState == null) {
-            //startService(new Intent(ACTION_CHANGE_VALUE, null, MainActivity.this,
-            //        ExampleIntentService.class));
-            //isStarted = true;
-        }*/
     }
-
-    /*@Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putBoolean(SERVICE_IS_STARTED, isStarted);
-        super.onSaveInstanceState(outState);
-    }*/
-
-    /*@Override
-    protected void onResume() {
-        super.onResume();
-        localBroadcastManager.registerReceiver(exampleBroadcastReceiver, intentFilter);
-    }*/
-
-    /*@Override
-    protected void onPause() {
-        super.onPause();
-        localBroadcastManager.unregisterReceiver(exampleBroadcastReceiver);
-    }*/
 
     @Override
     public void onFragmentInteraction() {
@@ -61,58 +36,18 @@ public class MainActivity extends FragmentActivity implements FragmentB.OnFragme
         } catch (RemoteException e) {
             e.printStackTrace();
         }
-        /*Fragment fragment = FragmentC.newInstance(editText.getText().toString());
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.textViewContainer, fragment);
-        fragmentTransaction.commit();*/
     }
-
-    /*public class EditViewCallbackImpl implements EditViewCallback {
-        @Override
-        public void onValueChanged(String value) {
-            editTextView.setText(value);
-        }
-    }*/
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
         public void onServiceConnected(ComponentName className,
                                        IBinder service) {
             remoteDataService = IRemoteDataService.Stub.asInterface(service);
             Toast.makeText(MainActivity.this, R.string.remote_service_connected, Toast.LENGTH_SHORT).show();
-            try {
-                Data data = remoteDataService.read();
-                TextView logOutput = findViewById(R.id.logOutput);;
-                logOutput.append(data.getValue());
-
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-            /*mService = new Messenger(service);
-            mCallbackText.append("Attached\n");
-            try {
-                Message msg = Message.obtain(null,
-                        RemoteService.MSG_REGISTER_CLIENT);
-                msg.replyTo = mMessenger;
-                mService.send(msg);
-
-                msg = Message.obtain(null,
-                        RemoteService.MSG_SET_VALUE, 0, 0);
-                Bundle data = new Bundle();
-                data.putString(MSG_SET_VALUE_FIELD, getResources().getString(R.string.hello_from_activity));
-                msg.setData(data);
-                mService.send(msg);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-
-            Toast.makeText(AdditionalActivity.this, R.string.example_service_connected,
-                    Toast.LENGTH_SHORT).show();*/
         }
 
         public void onServiceDisconnected(ComponentName className) {
-            remoteDataService = null;
-            //mCallbackText.append("Disconnected\n");
             Toast.makeText(MainActivity.this, R.string.remote_service_disconnected, Toast.LENGTH_SHORT).show();
+            remoteDataService = null;
         }
     };
 
@@ -123,11 +58,22 @@ public class MainActivity extends FragmentActivity implements FragmentB.OnFragme
         intent.setAction("ru.sberbank.lesson13.task.aidl.REMOTE_SERVICE_CALL");
         intent.setPackage("ru.sberbank.lesson13.task.aidl.service");
         bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+        scheduledThreadPool = Executors.newScheduledThreadPool(5);
+        scheduledThreadPool.scheduleWithFixedDelay(() -> {
+            try {
+                Data data = remoteDataService.read();
+                TextView logOutput = findViewById(R.id.logOutput);;
+                logOutput.setText(data.getValue());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, 0, 2, TimeUnit.SECONDS);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         unbindService(serviceConnection);
+        scheduledThreadPool.shutdown();
     }
 }
